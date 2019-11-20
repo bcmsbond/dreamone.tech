0a390548792433b1458bd8e632f4268391a7e98f
bcmsbond <bcmsbond@gmail.com>
2019-11-20 12:06:00 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const crypto = require("crypto");
+const config = require('./config');
+
+
+var app = express();
+
+//app.use(express.static("./public"));
+
+app.get('/', function(req, res){
+	var signature = req.query.signature,
+		timestamp = req.query.timestamp,
+		nonce = req.query.nonce,
+		echostr	= req.query.echostr;
+
+	console.log("Signature: " + signature);
+	console.log("Timestamp: " + timestamp);
+	console.log("Nonce: " + nonce);
+	console.log("Echostr: " + echostr);
+
+	var array = [config.token, timestamp, nonce];
+	array.sort();
+
+	var tempStr = array.join('');
+	const hashCode = crypto.createHash('sha1');
+	var resultCode = hashCode.update(tempStr,'utf8').digest('hex');
+
+	console.log("Verified Signature: " + resultCode);
+
+	if(resultCode === signature) {
+		res.send(echostr);
+	}
+	else {
+		res.send('mismatch');
+	}
+});
+
+
+app.listen(80);
+
+console.log("Server running on port 80");
+module.exports = app; // Export app instance
